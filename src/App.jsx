2d15d3041b76a0da742254b04e54ebdd86bfa87c
Jarl <jarl13.work@gmail.com>
2025-01-28 23:28:07 +0800

    add: install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import PostsList from "./features/posts/PostsList";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <div className="px-2 py-1">
         <div className="text-blue-500 text-3xl">
           React + <span className="text-red-700">Rails</span> Blog
         </div>
         <PostsList />
       </div>
-    </>
+    </Router>
   );
 }
 
